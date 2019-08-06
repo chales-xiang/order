3f73e65f4ddb7766ed98d114f248e824b976008c
xiangdezheng <xiangdezheng@nonobank.com>
2019-08-06 19:18:03 +0800

    origin/master

--- deploy.sh
+++ deploy.sh
@@ -31,6 +31,8 @@ rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
+echo "**************"
+
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
