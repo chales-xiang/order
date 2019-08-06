32d69bd0965d19400d936ee9e91bce41c649bea3
xiangdezheng <xiangdezheng@nonobank.com>
2019-08-06 18:13:04 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,6 @@ killTomcat()
       echo "no tomcat pid alive"
     else
       kill -9 $pid
-      echo "#################################################"
     fi
 }
 cd $PROJ_PATH/order
@@ -29,10 +28,8 @@ killTomcat
 
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-echo "*************$TOMCAT_APP_PATH/webapps/ROOT****************"
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
-echo "******$TOMCAT_APP_PATH**********"
 
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
